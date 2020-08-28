0efad4c5ffbe8449eed56833a35022be6497677b
seneark <seneark@protonmail.com>
2020-08-28 18:55:57 +0530

    Updates

---
+++ precache-manifest.e1326dd6182f45232168dac482f6eb76.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ac17104a513bbba69db66139d28803",
+    "url": "/in2truction/index.html"
+  },
+  {
+    "revision": "356fcd91334ebf7d921a",
+    "url": "/in2truction/static/css/2.6e59fa2c.chunk.css"
+  },
+  {
+    "revision": "8b58fb1333049ad78627",
+    "url": "/in2truction/static/css/main.e0031b11.chunk.css"
+  },
+  {
+    "revision": "356fcd91334ebf7d921a",
+    "url": "/in2truction/static/js/2.2e1c3b7f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/in2truction/static/js/2.2e1c3b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b58fb1333049ad78627",
+    "url": "/in2truction/static/js/main.fe504850.chunk.js"
+  },
+  {
+    "revision": "3b0d51ee979a90639f5e",
+    "url": "/in2truction/static/js/runtime-main.aa4f4a52.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/in2truction/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/in2truction/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/in2truction/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/in2truction/static/media/notification.c0d3c94c.eot"
+  }
+]);
